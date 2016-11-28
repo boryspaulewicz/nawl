ca711ea58f926282f85afc9c3d483f6c642aa4b3
Borysław Paulewicz <borys.paulewicz@gmail.com>
2016-11-28 11:01:03 +0100

    admin wybiera warunek

--- nawl.R
+++ nawl.R
@@ -126,8 +126,11 @@ i miesiąca urodzenia (np.  ms0706).
 ")
 gui.user.data()
 
-cnd = db.random.condition(c('same-emotion', 'diff-emotion', 'same-imagine', 'diff-imagine'))
-## cnd = gui.choose.item(c('same-emotion', 'diff-emotion', 'same-imagine', 'diff-imagine'))
+if(USER.DATA$name != 'admin'){
+  cnd = db.random.condition(c('same-emotion', 'diff-emotion', 'same-imagine', 'diff-imagine'))
+}else{
+  cnd = gui.choose.item(c('same-emotion', 'diff-emotion', 'same-imagine', 'diff-imagine'))
+}
 scale = str_split(cnd, '-')[[1]][2]
 
 gui.show.instruction(sprintf("
